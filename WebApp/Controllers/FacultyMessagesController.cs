f2bd1e382f75ca14511069cec3b00f5aaa42938d
Ashok Atkuri <31717364+S530458@users.noreply.github.com>
2018-11-29 15:47:54 -0600

    Add message faculty

--- WebApp/Controllers/FacultyMessagesController.cs
+++ WebApp/Controllers/FacultyMessagesController.cs
@@ -1,35 +1,27 @@
-﻿using Microsoft.Graph;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using WebApp.Helpers;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class StudentMessagesController : Controller
+    public class FacultyMessagesController : Controller
     {
         private OfficeHoursContext db = new OfficeHoursContext();
-        EventsService eventsService = new EventsService();
 
-        // GET: StudentMessages
-        public async Task<ActionResult> Index()
+        // GET: FacultyMessages
+        public ActionResult Index()
         {
-            // Initialize the GraphServiceClient.
-            GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
-            var userDetails = await eventsService.GetMyDetails(graphClient);
-
             List<StudentMessage> studentMessages = new List<StudentMessage>();
             var messages = db.messages.ToList();
             foreach (var msg in messages)
             {
-                if (msg.student_id.Equals(userDetails.Mail))
+                if (msg.Email.Equals(Session["facultyMail"].ToString()))
                 {
                     studentMessages.Add(msg);
                 }
@@ -37,7 +29,7 @@ namespace WebApp.Controllers
             return View(studentMessages);
         }
 
-        // GET: StudentMessages/Details/5
+        // GET: FacultyMessages/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -49,34 +41,29 @@ namespace WebApp.Controllers
             {
                 return HttpNotFound();
             }
+            studentMessage.is_archived = true;
+
+            db.Entry(studentMessage).State = EntityState.Modified;
+            db.SaveChanges();
             return View(studentMessage);
         }
 
-        // GET: StudentMessages/Create
+        // GET: FacultyMessages/Create
         public ActionResult Create()
         {
             ViewBag.Email = new SelectList(db.faculties, "Email", "first_Name");
             return View();
         }
 
-        // POST: StudentMessages/Create
+        // POST: FacultyMessages/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public async Task<ActionResult> Create([Bind(Include = "message")] StudentMessage studentMessage)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,student_id,student_Name,message,Date_Created,is_archived,Email")] StudentMessage studentMessage)
         {
-            // Initialize the GraphServiceClient.
-            GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
-            var userDetails = await eventsService.GetMyDetails(graphClient);
-
             if (ModelState.IsValid)
             {
-                studentMessage.Date_Created = DateTime.Now;
-                studentMessage.Email = Session["facultyMail"].ToString();
-                studentMessage.is_archived = false;
-                studentMessage.student_id = userDetails.Mail;
-                studentMessage.student_Name = userDetails.DisplayName;
-                
                 db.messages.Add(studentMessage);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -86,7 +73,7 @@ namespace WebApp.Controllers
             return View(studentMessage);
         }
 
-        // GET: StudentMessages/Edit/5
+        // GET: FacultyMessages/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -102,24 +89,15 @@ namespace WebApp.Controllers
             return View(studentMessage);
         }
 
-        // POST: StudentMessages/Edit/5
+        // POST: FacultyMessages/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,student_id,student_Name,message,Date_Created,is_archived,Email")] StudentMessage studentMessage)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,student_id,student_Name,message,Date_Created,is_archived,Email")] StudentMessage studentMessage)
         {
-            // Initialize the GraphServiceClient.
-            GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
-            var userDetails = await eventsService.GetMyDetails(graphClient);
-
             if (ModelState.IsValid)
             {
-                studentMessage.Date_Created = DateTime.Now;
-                studentMessage.Email = Session["facultyMail"].ToString();
-                studentMessage.is_archived = false;
-                studentMessage.student_id = userDetails.Mail;
-                studentMessage.student_Name = userDetails.DisplayName;
-
                 db.Entry(studentMessage).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -128,7 +106,7 @@ namespace WebApp.Controllers
             return View(studentMessage);
         }
 
-        // GET: StudentMessages/Delete/5
+        // GET: FacultyMessages/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -143,8 +121,9 @@ namespace WebApp.Controllers
             return View(studentMessage);
         }
 
-        // POST: StudentMessages/Delete/5
+        // POST: FacultyMessages/Delete/5
         [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
             StudentMessage studentMessage = db.messages.Find(id);
