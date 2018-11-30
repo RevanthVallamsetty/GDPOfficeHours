70a607751058207d3b02310022907c03cf739b5e
Ashok Atkuri <31717364+S530458@users.noreply.github.com>
2018-11-30 14:41:36 -0600

    Add tests models

---
+++ WebAppTests/Models/EventsServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.Tests
+{
+    [TestClass()]
+    public class EventsServiceTests
+    {
+        [TestMethod()]
+        public void GetMyDetailsTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetMyEventsTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetMyAppointmentsTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetMyCalendarViewTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void CreateEventTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void CreateStudentEventTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetEventTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void UpdateEventTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void DeleteEventTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void AcceptMeetingRequestTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ WebAppTests/Models/FilesServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.Tests
+{
+    [TestClass()]
+    public class FilesServiceTests
+    {
+        [TestMethod()]
+        public void GetMyFilesAndFoldersTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetSharedWithMeTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetMyDriveTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void CreateFileTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void CreateFolderTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void UploadLargeFileTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetFileOrFolderMetadataTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void DownloadFileTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void UpdateFileOrFolderMetadataTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void UpdateFileContentTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void DeleteFileOrFolderTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetSharingLinkTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
